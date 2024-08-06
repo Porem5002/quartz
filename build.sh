5f942437658ab4ad689b095b9c91177ef84e4f09
Carlos Bandeira <porem5002@gmail.com>
2024-08-06 02:10:59 +0100

    Added LICENSE Replaced build.sh with CMake

--- build.sh
+++
@@ -1,9 +0,0 @@
-compiler="g++"
-cppflags="-O2 -static -std=c++17 -Wall -Wextra -Wpedantic -D_CRT_SECURE_NO_WARNINGS"
-
-includedirs="-Ithird_party"
-$compiler $cppflags $includedirs quartz/quartz_impl.cpp -c -o quartz.obj
-
-libs="-luser32 -lopengl32 -lgdi32"
-includedirs="-Iquartz"
-$compiler $cppflags $includedirs examples/pong/*.cpp quartz.obj $libs -o pong.exe
